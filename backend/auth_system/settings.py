cd4aae62520711d6a5a9896fc77dc07a1aeaca84
Varun Kumar Valaboju <valav01@pfw.edu>
2023-11-25 23:45:09 -0500

    Allow cors settings

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -31,16 +31,22 @@ INSTALLED_APPS = [
     "djoser",
     "accounts",
     "rest_framework_simplejwt",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "auth_system.urls"
